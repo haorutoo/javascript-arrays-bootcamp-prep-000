68e6dcb3de55def8bf42ac22a3805b2cb2a13204
haorutoo <haorutoo@gmail.com>
2017-10-04 15:25:26 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,46 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array,element) {
+  var array = [element, ...array]
+  return array
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element) {
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array,element) {
+  var array = [...array,element]
+  return array
+}
+
+function destructivelyAddElementToEndOfArray(array,element) {
+  array =  array.push(element)
+  return array
+
+}
+
+function accessElementInArray(array,index){
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array = array.shift()
+  return array
+}
+
+function removeElementFromEndOfArray(array){
+  array = array.slice(-1) //(0, carray.length - 1)
+  return array
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array = array.pop()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array){
+  array = array.slice(1)
+  return array
+}
